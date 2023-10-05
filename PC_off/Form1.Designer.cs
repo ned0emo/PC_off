3a1c64da9751f76bc04bad8e6186efa29ec49e36
ned0emo <alexandr42suv@mail.ru>
2023-10-05 15:05:15 +0800

    Добавьте файлы проекта.

---
+++ PC_off/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace PC_off
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sevenBuildingEditButton = new System.Windows.Forms.Button();
+            this.firstBuildingEditButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.delayTextBox = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.firstBuildingTab = new System.Windows.Forms.TabPage();
+            this.refreshFirstCompsButton = new System.Windows.Forms.Button();
+            this.firstBuildingOffButton = new System.Windows.Forms.Button();
+            this.sevenBuildingTab = new System.Windows.Forms.TabPage();
+            this.refreshSevenCompsButton = new System.Windows.Forms.Button();
+            this.sevenBuildingOffButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.firstBuildingTab.SuspendLayout();
+            this.sevenBuildingTab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sevenBuildingEditButton
+            // 
+            this.sevenBuildingEditButton.Location = new System.Drawing.Point(471, 12);
+            this.sevenBuildingEditButton.Name = "sevenBuildingEditButton";
+            this.sevenBuildingEditButton.Size = new System.Drawing.Size(85, 23);
+            this.sevenBuildingEditButton.TabIndex = 0;
+            this.sevenBuildingEditButton.Text = "7 корпус";
+            this.sevenBuildingEditButton.UseVisualStyleBackColor = true;
+            this.sevenBuildingEditButton.Click += new System.EventHandler(this.sevenBuildingButton_Click);
+            // 
+            // firstBuildingEditButton
+            // 
+            this.firstBuildingEditButton.Location = new System.Drawing.Point(380, 12);
+            this.firstBuildingEditButton.Name = "firstBuildingEditButton";
+            this.firstBuildingEditButton.Size = new System.Drawing.Size(85, 23);
+            this.firstBuildingEditButton.TabIndex = 1;
+            this.firstBuildingEditButton.Text = "1 корпус";
+            this.firstBuildingEditButton.UseVisualStyleBackColor = true;
+            this.firstBuildingEditButton.Click += new System.EventHandler(this.firstBuildingButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Списки компов:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Задержка выключения (мин):";
+            // 
+            // delayTextBox
+            // 
+            this.delayTextBox.Location = new System.Drawing.Point(174, 12);
+            this.delayTextBox.MaxLength = 3;
+            this.delayTextBox.Name = "delayTextBox";
+            this.delayTextBox.Size = new System.Drawing.Size(100, 20);
+            this.delayTextBox.TabIndex = 4;
+            this.delayTextBox.TextChanged += new System.EventHandler(this.delayTextBox_TextChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.firstBuildingTab);
+            this.tabControl1.Controls.Add(this.sevenBuildingTab);
+            this.tabControl1.Location = new System.Drawing.Point(12, 38);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(548, 306);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // firstBuildingTab
+            // 
+            this.firstBuildingTab.AutoScroll = true;
+            this.firstBuildingTab.Controls.Add(this.refreshFirstCompsButton);
+            this.firstBuildingTab.Controls.Add(this.firstBuildingOffButton);
+            this.firstBuildingTab.Location = new System.Drawing.Point(4, 22);
+            this.firstBuildingTab.Name = "firstBuildingTab";
+            this.firstBuildingTab.Padding = new System.Windows.Forms.Padding(3);
+            this.firstBuildingTab.Size = new System.Drawing.Size(540, 280);
+            this.firstBuildingTab.TabIndex = 0;
+            this.firstBuildingTab.Text = "1 корпус";
+            this.firstBuildingTab.UseVisualStyleBackColor = true;
+            // 
+            // refreshFirstCompsButton
+            // 
+            this.refreshFirstCompsButton.Location = new System.Drawing.Point(125, 6);
+            this.refreshFirstCompsButton.Name = "refreshFirstCompsButton";
+            this.refreshFirstCompsButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshFirstCompsButton.TabIndex = 1;
+            this.refreshFirstCompsButton.Text = "Обновить";
+            this.refreshFirstCompsButton.UseVisualStyleBackColor = true;
+            // 
+            // firstBuildingOffButton
+            // 
+            this.firstBuildingOffButton.Location = new System.Drawing.Point(6, 6);
+            this.firstBuildingOffButton.Name = "firstBuildingOffButton";
+            this.firstBuildingOffButton.Size = new System.Drawing.Size(113, 23);
+            this.firstBuildingOffButton.TabIndex = 0;
+            this.firstBuildingOffButton.Text = "Все выделенное";
+            this.firstBuildingOffButton.UseVisualStyleBackColor = true;
+            // 
+            // sevenBuildingTab
+            // 
+            this.sevenBuildingTab.AutoScroll = true;
+            this.sevenBuildingTab.Controls.Add(this.refreshSevenCompsButton);
+            this.sevenBuildingTab.Controls.Add(this.sevenBuildingOffButton);
+            this.sevenBuildingTab.Location = new System.Drawing.Point(4, 22);
+            this.sevenBuildingTab.Name = "sevenBuildingTab";
+            this.sevenBuildingTab.Padding = new System.Windows.Forms.Padding(3);
+            this.sevenBuildingTab.Size = new System.Drawing.Size(540, 280);
+            this.sevenBuildingTab.TabIndex = 1;
+            this.sevenBuildingTab.Text = "7 корпус";
+            this.sevenBuildingTab.UseVisualStyleBackColor = true;
+            // 
+            // refreshSevenCompsButton
+            // 
+            this.refreshSevenCompsButton.Location = new System.Drawing.Point(125, 6);
+            this.refreshSevenCompsButton.Name = "refreshSevenCompsButton";
+            this.refreshSevenCompsButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshSevenCompsButton.TabIndex = 2;
+            this.refreshSevenCompsButton.Text = "Обновить";
+            this.refreshSevenCompsButton.UseVisualStyleBackColor = true;
+            // 
+            // sevenBuildingOffButton
+            // 
+            this.sevenBuildingOffButton.Location = new System.Drawing.Point(6, 6);
+            this.sevenBuildingOffButton.Name = "sevenBuildingOffButton";
+            this.sevenBuildingOffButton.Size = new System.Drawing.Size(113, 23);
+            this.sevenBuildingOffButton.TabIndex = 1;
+            this.sevenBuildingOffButton.Text = "Все выделенное";
+            this.sevenBuildingOffButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(290, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Для применения изменений требуется перезапуск";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 356);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.delayTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.firstBuildingEditButton);
+            this.Controls.Add(this.sevenBuildingEditButton);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Отключение компов (1.0)";
+            this.tabControl1.ResumeLayout(false);
+            this.firstBuildingTab.ResumeLayout(false);
+            this.sevenBuildingTab.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sevenBuildingEditButton;
+        private System.Windows.Forms.Button firstBuildingEditButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox delayTextBox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage sevenBuildingTab;
+        private System.Windows.Forms.Button firstBuildingOffButton;
+        private System.Windows.Forms.Button sevenBuildingOffButton;
+        public System.Windows.Forms.TabPage firstBuildingTab;
+        private System.Windows.Forms.Button refreshFirstCompsButton;
+        private System.Windows.Forms.Button refreshSevenCompsButton;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PC_off/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PC_off
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, List<string>> firstBuildingComps;
+        private Dictionary<string, List<string>> sevenBuildingComps;
+
+        private bool isCompsLoading;
+        private bool isNotepadRunning;
+
+        private int completeCounter;
+
+        public Form1()
+        {
+            isNotepadRunning = false;
+            isCompsLoading = false;
+            completeCounter = 0;
+
+            firstBuildingComps = new Dictionary<string, List<string>>();
+            sevenBuildingComps = new Dictionary<string, List<string>>();
+
+            InitializeComponent();
+
+            changeAllButtonStatus(false);
+
+            loadCompsList("1", firstBuildingComps, firstBuildingTab, refreshFirstCompsButton, firstBuildingOffButton);
+            loadCompsList("7", sevenBuildingComps, sevenBuildingTab, refreshSevenCompsButton, sevenBuildingOffButton);
+        }
+
+        private async void loadCompsList(string building, Dictionary<string, List<string>> buildingsComps, TabPage tab, Button refreshButton, Button offAllButton)
+        {
+            isCompsLoading = true;
+            string domainName = await loadDomainName();
+
+            var buttonList = new List<Button>();
+            var taskList = new List<Task>();
+            var fullBuildingCheckBoxList = new List<CheckBox>();
+
+            StreamReader sr = null;
+
+            try
+            {
+                sr = new StreamReader($"./{building}.txt");
+                var file = await sr.ReadToEndAsync();
+                sr.Close();
+
+                var classesList = Regex.Replace(file, "#.*\n", "").Split(';').Skip(1);
+
+                foreach (var classes in classesList)
+                {
+                    var comps = Regex.Split(classes, "\\s+");
+
+                    string className = comps[0];
+
+                    buildingsComps.Add(className, new List<string>());
+
+                    for (int i = 1; i < comps.Length; i++)
+                    {
+                        if (comps[i].Length > 0)
+                        {
+                            buildingsComps[className].Add(comps[i]);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < buildingsComps.Count; i++)
+                {
+                    var key = buildingsComps.Keys.ElementAt(i);
+                    Button button = new Button()
+                    {
+                        Text = key,
+                        Location = new Point(6 + i * 90, 40),
+                        Enabled = false,
+                    };
+                    tab.Controls.Add(button);
+                    buttonList.Add(button);
+
+                    var checkBoxList = new List<CheckBox>();
+                    for (int j = 0; j < buildingsComps[key].Count; j++)
+                    {
+                        CheckBox cb = new CheckBox()
+                        {
+                            Text = buildingsComps[key][j],
+                            Location = new Point(6 + i * 90, 70 + j * 20),
+                            Width = 80,
+                            Enabled = false,
+                        };
+                        checkBoxList.Add(cb);
+                        fullBuildingCheckBoxList.Add(cb);
+
+                        tab.Controls.Add(cb);
+                    }
+
+                    foreach (CheckBox cb in checkBoxList)
+                    {
+                        taskList.Add(checkPing(cb));
+                    }
+
+                    ///
+                    /// Кнопка выключения аудитории
+                    ///
+                    button.Click += (s, e) =>
+                    {
+                        if (isCompsLoading)
+                        {
+                            MessageBox.Show($"Необходимо дождаться загрузки", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return;
+                        }
+
+                        var checkedComps = checkBoxList.FindAll(cb => cb.Checked);
+                        if (checkedComps.Count < 1) return;
+
+                        int delay = -1;
+                        try
+                        {
+                            delay = Convert.ToInt32(delayTextBox.Text);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Ошибка применения задержки выключения\r\n{ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+
+                        if (MessageBox.Show($"Следующие компьютеры будут выключены: " +
+                            $"{checkedComps.Select((cb) => cb.Text).Aggregate((n1, n2) => $"{n1}, {n2}")}",
+                            "Внимание", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == DialogResult.Cancel)
+                        {
+                            return;
+                        }
+
+                        foreach (CheckBox cb in checkedComps)
+                        {
+                            Process.Start("cmd.exe", $"/C shutdown /s /t {delayTextBox.Text} /m \\\\{cb.Text}{domainName}");
+                        }
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                sr?.Close();
+                MessageBox.Show($"Ошибка обработки компов\r\n{ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            foreach (var task in taskList) await task;
+            foreach (var button in buttonList) button.Enabled = true;
+
+            ///
+            /// Кнопка обновления
+            ///
+            refreshButton.Click += async (s, e) =>
+            {
+                if (isCompsLoading)
+                {
+                    MessageBox.Show($"Необходимо дождаться загрузки", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                isCompsLoading = true;
+                taskList.Clear();
+                foreach (var button in buttonList) button.Enabled = false;
+
+                foreach (var cb in fullBuildingCheckBoxList)
+                {
+                    cb.Enabled = false;
+                    taskList.Add(checkPing(cb));
+                }
+
+                foreach (var task in taskList) await task;
+                foreach (var button in buttonList) button.Enabled = true;
+
+                isCompsLoading = false;
+            };
+
+            ///
+            /// Кнопка выключения всего выделенного
+            ///
+            offAllButton.Click += (s, e) =>
+            {
+                if (isCompsLoading)
+                {
+                    MessageBox.Show($"Необходимо дождаться загрузки", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                var checkedComps = fullBuildingCheckBoxList.FindAll(cb => cb.Checked);
+                if (checkedComps.Count < 1) return;
+
+                if (MessageBox.Show($"Следующие компьютеры будут выключены: " +
+                    $"{checkedComps.Select((cb) => cb.Text).Aggregate((n1, n2) => $"{n1}, {n2}")}",
+                    "Внимание", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == DialogResult.Cancel)
+                {
+                    return;
+                }
+
+                foreach (CheckBox cb in checkedComps)
+                {
+                    Process.Start("cmd.exe", $"/C shutdown /s /t {delayTextBox.Text} /m \\\\{cb.Text}{domainName}");
+                }
+            };
+
+            isCompsLoading = false;
+            completeCounter++;
+
+            if (completeCounter > 1)
+            {
+                changeAllButtonStatus(true);
+            }
+        }
+
+        private async Task<string> loadDomainName()
+        {
+            string tmp = "";
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader("./domain.txt");
+                tmp = "." + Regex.Replace(await sr.ReadToEndAsync(), "\\s+", "");
+                sr.Close();
+            }
+            catch
+            {
+                sr?.Close();
+            }
+
+            return tmp;
+        }
+
+        private async Task checkPing(CheckBox cb)
+        {
+            var ping = new Ping();
+
+            try
+            {
+                if ((await ping.SendPingAsync(cb.Text, 2)).Status == IPStatus.Success)
+                {
+                    cb.AutoCheck = true;
+                    cb.Checked = true;
+                    cb.ForeColor = Color.Green;
+                }
+                else
+                {
+                    cb.AutoCheck = false;
+                    cb.ForeColor = Color.Red;
+                }
+            }
+            catch
+            {
+                cb.AutoCheck = false;
+                cb.ForeColor = Color.Red;
+            }
+
+            cb.Enabled = true;
+        }
+
+        private void delayTextBox_TextChanged(object sender, EventArgs e) => delayTextBox.Text = Regex.Replace(delayTextBox.Text, "\\D+", "");
+
+        private void firstBuildingButton_Click(object sender, EventArgs e) => editClassrooms("1");
+
+        private void sevenBuildingButton_Click(object sender, EventArgs e) => editClassrooms("7");
+
+        private void editClassrooms(string fileName)
+        {
+            if (isNotepadRunning) return;
+            isNotepadRunning = true;
+
+            Task.Run(new Action(() =>
+            {
+                try
+                {
+                    using (Process pProcess = new Process())
+                    {
+                        pProcess.StartInfo.FileName = @"notepad";
+                        pProcess.StartInfo.Arguments = Application.StartupPath + $"/{fileName}.txt";
+                        pProcess.Start();
+                        pProcess.WaitForExit();
+
+                        isNotepadRunning = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    isNotepadRunning = false;
+                    MessageBox.Show($"Ошибка открытия блокнота\n{ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }));
+        }
+
+        private void changeAllButtonStatus(bool enabled)
+        {
+            firstBuildingEditButton.Enabled = enabled;
+            sevenBuildingEditButton.Enabled = enabled;
+            refreshFirstCompsButton.Enabled = enabled;
+            refreshSevenCompsButton.Enabled = enabled;
+            firstBuildingOffButton.Enabled = enabled;
+            sevenBuildingOffButton.Enabled = enabled;
+        }
+    }
+}
